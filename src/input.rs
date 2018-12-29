e4971aa10804d7360f3ecc351fde6c2946bfb995
Joe Clay <27cupsofcoffee@gmail.com>
2018-12-29 15:22:13 +0000

    Add note to docs about gamepad indexes

--- src/input.rs
+++ src/input.rs
@@ -1,4 +1,12 @@
 //! Functions and types relating to handling user input (e.g. keyboards, mice, gamepads).
+//!
+//! # Gamepads
+//!
+//! When accessing gamepad state, you specify which gamepad you're interested in via a 'gamepad index'.
+//! The first gamepad connected to the system has index 0, the second has index 1, and so on.
+//!
+//! If a controller is disconnected, the next controller to be connected will take its index - otherwise,
+//! a new one will be allocated. This behaviour might be made smarter in future versions.
 
 use fnv::{FnvHashMap, FnvHashSet};
 use glm::Vec2;
