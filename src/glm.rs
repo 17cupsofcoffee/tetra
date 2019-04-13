a41b9ba8bb3ba727d0d28550c04f0cd9c342ee00
Joe Clay <27cupsofcoffee@gmail.com>
2019-04-13 15:33:18 +0100

    Update GLM docs

--- src/glm.rs
+++ src/glm.rs
@@ -1,11 +1,9 @@
 //! Functions and types relating to math (provided by the nalgebra-glm crate).
 //!
-//! This module re-exports the contents of the `nalgebra-glm` crate.
-//!
 //! Ideally, the documentation for `nalgebra-glm` would be reproduced here for your convienence. Unfortunately,
 //! [rustdoc currently doesn't handle this very well](https://github.com/rust-lang/rust/issues/15823), and the
 //! re-exported docs would likely be more confusing than helpful. Until those issues are fixed, you can find
-//! the documentation for `nalgebra-glm` on [their website](https://www.nalgebra.org/rustdoc_glm/nalgebra_glm/index.html).
+//! the documentation for `nalgebra-glm` by clicking the re-export link below.
 
 #[doc(no_inline)]
 pub use nalgebra_glm::*;
